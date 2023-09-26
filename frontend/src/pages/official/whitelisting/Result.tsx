e258ac6f2e16aebf784063a51fd3b37ea23c2acd
Azlan <azlan@thuleen.io>
2023-09-26 18:49:38 +0800

    Added Result component for Whitelisting in Officer

---
+++ frontend/src/pages/official/whitelisting/Result.tsx
@@ -0,0 +1,35 @@
+import {
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+} from '@mui/material';
+import { useOfficialDispatch } from '../../../services/hook';
+import { resetWhitelistSubmission } from '../../../services/official/reducer';
+
+export default function Result(props: { message: string }) {
+  const dispatch = useOfficialDispatch();
+
+  const handleClose = () => {
+    dispatch(resetWhitelistSubmission());
+  };
+
+  return (
+    <Box sx={{ margin: 3 }}>
+      <Card sx={{ minWidth: 275, backgroundColor: '#f5f6fa' }}>
+        <CardHeader
+          title="Result"
+          subheader="Whitelisting"
+          sx={{ color: '#273c75' }}
+        />
+        <CardContent>{props.message}</CardContent>
+        <CardActions sx={{ display: 'flex', flexDirection: 'row' }}>
+          <Box sx={{ flexGrow: 1 }} />
+          <Button onClick={handleClose}>close</Button>
+        </CardActions>
+      </Card>
+    </Box>
+  );
+}
--- frontend/src/pages/official/whitelisting/Whitelisting.tsx
+++ frontend/src/pages/official/whitelisting/Whitelisting.tsx
@@ -2,10 +2,23 @@ import Box from '@mui/material/Box';
 import FormHeader from '../../../commons/FormHeader';
 import styles from '../residency/styles';
 import WhitelistingForm from './WhitelistingForm';
+import { useOfficialSelector } from '../../../services/hook';
+import { OfficialState } from '../../../services/store';
+import Result from './Result';
+import BackdropLoader from '../../../commons/BackdropLoader';
 
 export default function Whitelisting() {
+  const { submissionState, submissionMsg } = useOfficialSelector(
+    (state: OfficialState) => state.official
+  );
+
+  if (submissionState === 'OK' && submissionMsg) {
+    return <Result message={submissionMsg} />;
+  }
+
   return (
     <Box sx={{ ...styles.container, margin: 3 }}>
+      <BackdropLoader submissionState={submissionState} />
       <FormHeader title="Whitelisting" />
       <WhitelistingForm />
     </Box>
