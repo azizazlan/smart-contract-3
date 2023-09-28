6b7d5d49d3ee6e7dd75e3c458da9377d1785257f
Azlan <azlan@thuleen.io>
2023-09-29 01:54:44 +0800

    Updated disclaimer in the About page

--- frontend/src/pages/about/About.tsx
+++ frontend/src/pages/about/About.tsx
@@ -1,7 +1,8 @@
 import Box from '@mui/material/Box';
 import styles from './styles';
-import { Typography } from '@mui/material';
+import { Alert, Typography } from '@mui/material';
 import flag from '../../assets/malacca-flag.png';
+import truncateEthAddr from '../../utils/truncateEthAddr';
 
 const MELAKA_RESIDENT_CONTRACT_ADDR = import.meta.env
   .VITE_APP_ADDR_MLK_RESIDENT;
@@ -19,8 +20,16 @@ export default function About() {
           width: '100%',
         }}
       >
-        <img src={flag} alt="melaka flag" style={{ width: '275px' }} />
+        <img src={flag} alt="melaka flag" style={{ width: '175px' }} />
       </Box>
+      <Alert
+        severity="warning"
+        icon={false}
+        sx={{ marginBottom: 2, marginTop: -2, zIndex: -10 }}
+      >
+        This is <b>not</b> a production release, and should only be used for
+        demo and testing.
+      </Alert>
       <Typography variant="body2" gutterBottom>
         Melaka EquiSubsidy is a basic DApp to demonstrate the use of smart
         contracts to regulate the distribution of subsidised items to the
@@ -31,18 +40,18 @@ export default function About() {
         Smart contract MelakaResident ERC721:
       </Typography>
       <Typography style={{ fontFamily: 'Abel', fontSize: '12.5pt' }}>
-        {MELAKA_RESIDENT_CONTRACT_ADDR}
+        {truncateEthAddr(MELAKA_RESIDENT_CONTRACT_ADDR)}
       </Typography>
       <br />
       <Typography variant="body2" gutterBottom>
         Smart contract MelakaRice ERC20:
       </Typography>
       <Typography style={{ fontFamily: 'Abel', fontSize: '12.5pt' }}>
-        {MELAKA_RICE_CONTRACT_ADDR}
+        {truncateEthAddr(MELAKA_RICE_CONTRACT_ADDR)}
       </Typography>
       <br />
       <Typography variant="body2" gutterBottom>
-        Built for,
+        Demo solution built for,
         <br />
         <b>Jabatan Ketua Menteri Melaka, Malaysia.</b>
         <br />
